9a804b39cbc041bb5a12a0ce6690d44d9c6e998a
niry <loloniry@yahoo.fr>
2018-01-11 15:35:10 +0100

    n

--- Grand Hotel/UIL/Pages/PageClients.cs
+++ Grand Hotel/UIL/Pages/PageClients.cs
@@ -7,45 +7,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace UIL
+
+//GESTION DES CLIENTS
 {
     class PageClients : MenuPage
     {
         private IList<Client> _clients;
         private Client _client;
-        public PageClients(): base("Page Clients")
+        public PageClients() : base("Page Clients")
         {
             Menu.AddOption("1", "Liste des cLients", AfficherClients);
             Menu.AddOption("2", "Coordonnées du client", InfoClient);
             Menu.AddOption("3", "Saisir un nouveau client", SaisirClient);
-
-
         }
-
-        private void SaisirClient()
+        //-----------------------------------------------------------------------------
+        //1-Affichage de la liste des clients
+        private void AfficherClients()
         {
-            Output.WriteLine("Saisissez les informations du nouveau client :");
-            Client cli = new Client();
-            cli.Civilite = Input.Read<string>("Civilité (M/Mlle/Mme) :");
-            cli.Nom= Input.Read<string>("Nom :");
-            cli.Prenom= Input.Read<string>("Prenom");
-            cli.CarteFidelite= Input.Read<bool>("0 (Non) ou 1 (Oui) :");
-            cli.Societe= Input.Read<string>("Nom (si renseigné) :");
-            Output.WriteLine("Voulez-vous entrer l'adresse du nouveau client : O/N");
-            string choix = Console.ReadLine();
-            if (choix=="O")
-            {
-                Adresse ad = new Adresse();
-                ad.RueEtComplement = Input.Read<string>("Rue et complément :");
-                ad.CodePostal = Input.Read<string>("Code Postal:");
-                ad.Ville = Input.Read<string>("Ville :");
-            }
-            else Enregister();
+            _clients = Metier.GetClients();
+            ConsoleTable.From(_clients).Display("Clients");
         }
 
+        //----------------------------------------------------------------------
+        //2-Coordonnées du client
         private void InfoClient()
         {
             int Id = Input.Read<int>("Veuillez saisir l'identifiant du client pour ses coordonnées: ");
-            _client =  (Client)Metier.GetCLient(Id);
+            _client = (Client)Metier.GetCLient(Id);
 
             List<Client> Clientliste = new List<Client>();
             Clientliste.Add(_client);
@@ -53,13 +41,53 @@ namespace UIL
 
             ConsoleTable.From(Clientliste).Display("Coordonnées client: ");
         }
-
-
-        //Affichage de la liste des clients
-        private void AfficherClients()
+        //----------------------------------------------------------------------
+        //3-Saisir un nouveau client
+        private void SaisirClient()
         {
-            _clients = Metier.GetClients();
-            ConsoleTable.From(_clients).Display("Clients");
+            Output.WriteLine("Voulez-vous entrer un nouveau client : \n1.Oui \n2.Non");
+            string saisieClient = Console.ReadLine();
+            bool saisie = false;
+            switch (saisieClient)
+            {
+                case "1":
+                    saisie = true;
+                    break;
+                case "2":
+                    saisie = false;
+                    break;
+                default:
+                    Output.WriteLine("Erreur de saisie!");
+                    break;
+            }
+
+
+            Output.WriteLine("Saisissez les informations du nouveau client :");
+            Client cli = new Client();
+            cli.Civilite = Input.Read<string>("Civilité (M/Mlle/Mme) :");
+            cli.Nom = Input.Read<string>("Nom :");
+            cli.Prenom = Input.Read<string>("Prenom");
+            cli.CarteFidelite = Input.Read<bool>("0 (Non) ou 1 (Oui) :");
+            cli.Societe = Input.Read<string>("Nom (si renseigné) :");
+
+            //Saisie adresse client
+            Output.WriteLine("Voulez-vous entrer l'adresse du nouveau client : O/N");
+            string choix = Console.ReadLine();
+            if (choix == "O")
+            {
+                Adresse ad = new Adresse();
+                ad.RueEtComplement = Input.Read<string>("Rue et complément :");
+                ad.CodePostal = Input.Read<string>("Code Postal:");
+                ad.Ville = Input.Read<string>("Ville :");
+
+            }
+            else Enregister();
+
+            if (!Enregister())
+                Output.WriteLine(ConsoleColor.Blue, "Enregistrement du nouveau client avec succès");
+            else
+                Output.WriteLine(ConsoleColor.Red, "Erreur d'enregistrement!!!");
         }
+
     }
 }
