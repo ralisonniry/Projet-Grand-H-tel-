689776258803e0e14d64ed9b680988b888be6abd
denis <Adminl@PARME44>
2018-01-11 13:45:34 +0100

    dd

--- Grand Hotel/DAL/Facture.cs
+++ Grand Hotel/DAL/Facture.cs
@@ -12,7 +12,7 @@ namespace DAL
     using System;
     using System.Collections.Generic;
     
-    public partial class Facture
+    public class Facture
     {
         public Facture()
         {
