6dab97ab5e7a68282d36ac0d0ac9a65d0938af23
denis <Adminl@PARME44>
2018-01-11 11:25:52 +0100

    rr

--- Grand Hotel/ConsoleApp1/Reservation.cs
+++ Grand Hotel/ConsoleApp1/Reservation.cs
@@ -8,9 +8,12 @@ namespace BOL
 {
     public class Reservation
     {
-
-
-
+        public int IdClient { get; set; }       // Id du client proprietaire de la facture -> permet de trouver la facture selon le client
+        public DateTime Jour { get; set; }      // Jour de la reservation
+        public int NumeroChambre { get; set; }
+        public int NbPersonnes { get; set; }
+        public int HeureArrivee { get; set; }
+        public int Travail { get; set; }            // 0 : non renseigné  / 1 : personnel / 2 : travail
     }
 
 
@@ -20,6 +23,9 @@ namespace BOL
     public class Chambre
     {
 
+
+
+        public List<Reservation> ReservationDeLaChambre { get; set; }       // reservations effectuées pour cette chambre
 
     }
 
