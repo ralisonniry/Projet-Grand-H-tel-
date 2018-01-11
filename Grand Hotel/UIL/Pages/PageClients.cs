65189513450760b3decd44f602f10a898a48b48e
niry <loloniry@yahoo.fr>
2018-01-11 14:35:14 +0100

    n

--- Grand Hotel/UIL/Pages/PageClients.cs
+++ Grand Hotel/UIL/Pages/PageClients.cs
@@ -30,11 +30,16 @@ namespace UIL
             cli.Prenom= Input.Read<string>("Prenom");
             cli.CarteFidelite= Input.Read<bool>("0 (Non) ou 1 (Oui) :");
             cli.Societe= Input.Read<string>("Nom (si renseigné) :");
-            Output.WriteLine("Voulez-vous entrer l'adresse du nouveau client :");
-            Adresse ad = new Adresse();
-            ad.RueEtComplement= Input.Read<string>("Rue et complément :");
-            ad.CodePostal= Input.Read<string>("Code Postal:");
-            ad.Ville= Input.Read<string>("Ville :");
+            Output.WriteLine("Voulez-vous entrer l'adresse du nouveau client : O/N");
+            string choix = Console.ReadLine();
+            if (choix=="O")
+            {
+                Adresse ad = new Adresse();
+                ad.RueEtComplement = Input.Read<string>("Rue et complément :");
+                ad.CodePostal = Input.Read<string>("Code Postal:");
+                ad.Ville = Input.Read<string>("Ville :");
+            }
+            else Enregister();
         }
 
         private void InfoClient()
